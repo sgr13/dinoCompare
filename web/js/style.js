228c00e007c8c0e4d0acd60c41ed94eb767324d1
sgr13 <sgr13@student.edu>
2017-07-15 14:55:37 +0200

    progress bar animation

--- web/js/style.js
+++ web/js/style.js
@@ -1,61 +1,96 @@
 $(document).ready(function () {
 
 //--------------------------
-    $('#weight .progres-bar').css('width', '0%')
-    $('#weight .progres-bar').animate({width: 88 + '%'}, 2000);
+    $('#weight .progres-bar').css('width', '0%');
+    $('#weight .progress-bar180').css('width', '100%');
+    $('#lenght .progres-bar').css('width', '0%');
+    $('#lenght .progress-bar180').css('width', '100%');
+    $('#height .progres-bar').css('width', '0%');
+    $('#height .progress-bar180').css('width', '100%');
 
-    $first = ($("#weight").children().children().html());
-    $second = ($("#weight").children().next().next().children().html());
+    $(window).scroll(function() {
+        var bottomPos = $(window).scrollTop() + $(window).height();
+        var weightRowPosition = $('#weight').offset().top;
 
-    $first = parseFloat($first);
-    $second = parseFloat($second);
+        if (bottomPos > weightRowPosition) {
+            var progresBar180 = $('.progress-bar180').attr('percentValue');
+            var progresBar = $('.progres-bar').attr('percentValue');
 
-    if ($first > $second) {
-        $('#weight .progress180').css("background-color", "yellowgreen");
-    } else if ($first < $second) {
-        $('#weight .progres-bar').css("background-color", "yellowgreen");
+            $('#weight .progres-bar').animate({width:progresBar  + '%'}, 2000);
+            $('#weight .progress-bar180').animate({width: progresBar180 + '%'}, 2000, function() {
+                $first = parseFloat($("#weight").children().children().html());
+                $second = parseFloat($("#weight").children().next().next().children().html());
 
-    } else {
-        $('#weight .progress180').css("background-color", "yellowgreen");
-        $('#weight .progress-bar').css("background-color", "yellowgreen");
-    }
+                if ($first > $second) {
+                    $('#weight .progress180').css("background-color", "yellowgreen");
+                    $('#weight .progres-bar').css("background-color", "red");
 
-//--------------------------
+                } else if ($first < $second) {
+                    $('#weight .progres-bar').css("background-color", "yellowgreen");
+                    $('#weight .progress180').css("background-color", "red");
 
-    $first = ($("#lenght").children().children().html());
-    $second = ($("#lenght").children().next().next().children().html());
+                } else {
+                    $('#weight .progress180').css("background-color", "yellowgreen");
+                    $('#weight .progres-bar').css("background-color", "yellowgreen");
+                }
+            });
+        }
+        var lenghtRowPosition = $('#lenght').offset().top;
 
-    $first = parseFloat($first);
-    $second = parseFloat($second);
+        if (bottomPos > lenghtRowPosition) {
+            var progresBar180 = $('#lenght .progress-bar180').attr('percentValue');
+            var progresBar = $('#lenght .progres-bar').attr('percentValue');
 
-    if ($first > $second) {
-        $("#lenght").children().first().addClass('winner');
-    } else if ($first < $second) {
-        $("#lenght").children().next().next().addClass('winner');
-    } else {
-        $("#lenght").children().addClass('winner');
-        $("#lenght").children().next().next().children().addClass('winner');
-    }
+            $('#lenght .progres-bar').animate({width:progresBar  + '%'}, 2000);
 
+            $('#lenght .progress-bar180').animate({width: progresBar180 + '%'}, 2000, function() {
+                $first = parseFloat($("#lenght").children().children().html());
+                $second = parseFloat($("#lenght").children().next().next().children().html());
 
-//-------------------------
+                if ($first > $second) {
+                    $('#lenght .progress180').css("background-color", "yellowgreen");
+                    $('#lenght .progres-bar').css("background-color", "red");
 
-    $first = ($("#height").children().children().html());
-    $second = ($("#height").children().next().next().children().html());
+                } else if ($first < $second) {
+                    $('#lenght .progres-bar').css("background-color", "yellowgreen");
+                    $('#lenght .progress180').css("background-color", "red");
 
-    $first = parseFloat($first);
-    $second = parseFloat($second);
+                } else {
+                    $('#lenght .progress180').css("background-color", "yellowgreen");
+                    $('#lenght .progres-bar').css("background-color", "yellowgreen");
+                }
+            });
+        }
 
-    if ($first > $second) {
-        $("#height").children().first().addClass('winner');
-    } else if ($first < $second) {
-        $("#height").children().next().next().addClass('winner');
-    } else {
-        $("#height").children().addClass('winner');
-        $("#height").children().next().next().addClass('winner');
-    }
+        var heightRowPosition = $('#height').offset().top;
 
-    //-----------------------
+        if (bottomPos > heightRowPosition) {
+            var progresBar180 = $('#height .progress-bar180').attr('percentValue');
+            var progresBar = $('#height .progres-bar').attr('percentValue');
+
+            $('#height .progres-bar').animate({width:progresBar  + '%'}, 2000);
+
+            $('#height .progress-bar180').animate({width: progresBar180 + '%'}, 2000, function() {
+                $first = parseFloat($("#height").children().children().html());
+                $second = parseFloat($("#height").children().next().next().children().html());
+
+                if ($first > $second) {
+                    $('#height .progress180').css("background-color", "yellowgreen");
+                    $('#height .progres-bar').css("background-color", "red");
+
+                } else if ($first < $second) {
+                    $('#height .progres-bar').css("background-color", "yellowgreen");
+                    $('#height .progress180').css("background-color", "red");
+
+                } else {
+                    $('#height .progress180').css("background-color", "yellowgreen");
+                    $('#height .progres-bar').css("background-color", "yellowgreen");
+                }
+            });
+        }
+
+    }).scroll();
+
 
     $('.imgSelectedDino').mouseover(function () {
         console.log("Działa");
